45fac602e14bc6d7bacc2a445e88a4aa6755078a
ChenYenRu <rebecca946487@gmail.com>
2025-03-31 03:31:53 +0000

    feat: Changed list and json support to pydantic object

--- sceneprogllm/llm.py
+++ sceneprogllm/llm.py
@@ -10,16 +10,24 @@ Key features
 import os
 import logging
 import json
+from pydantic import BaseModel, Json, create_model
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.output_parsers import StrOutputParser, SimpleJsonOutputParser, CommaSeparatedListOutputParser, PydanticOutputParser
 from langchain_ollama import ChatOllama
+
 from .cache_manager import CacheManager
 from .image_helper import ImageHelper
 from .text2img import text2imgSD, text2imgOpenAI
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='sceneprogllm.log', encoding='utf-8', level=logging.INFO)
+
+class ListResponse(BaseModel):
+    response: list[str]
+
+class DefaultJsonResponse(BaseModel):
+    response: str
 
 class LLM:
     def __init__(self,
@@ -81,18 +89,27 @@ class LLM:
         if '{' in query:
             query = query.replace('{', '{{').replace('}', '}}')
         ## sanitize system description
-        if '{' in system_desc:
+        if '{' in self.system_desc:
             system_desc = system_desc.replace('{', '{{').replace('}', '}}')
         
         if self.response_format == "3d":
             from .textto3d import text_to_3d
             return text_to_3d(query)
-        
-        if self.response_format == "pydantic":
+        elif self.response_format == "pydantic":
             assert pydantic_object, "Pydantic object is required for response format 'pydantic'"
-            
-        if self.response_format == "image":
+        elif self.response_format == "image":
             return self.text2img(query)
+        elif self.response_format == "list":
+            pydantic_object = ListResponse
+        elif self.response_format == "json":
+            if self.json_keys is not None:
+                CustomJSONModel = create_model(
+                    'CustomJSONModel',
+                    **{key: (str, ...) for key in self.json_keys}
+                )
+                pydantic_object = CustomJSONModel
+            else:
+                pydantic_object = DefaultJsonResponse
         
         if self.use_cache and not image_paths:
             result = self.cache.respond(query)
@@ -105,44 +122,22 @@ class LLM:
         
         full_prompt = self._get_prompt_with_history()
         
-        if self.response_format == "json":
-            chain = self.prompt_template | self.model | SimpleJsonOutputParser()
-        elif self.response_format == "list":
-            chain = self.prompt_template | self.model | CommaSeparatedListOutputParser()
-        elif self.response_format == "pydantic":
+        if pydantic_object is not None:
             parser = PydanticOutputParser(pydantic_object=pydantic_object)
             self.prompt_template = PromptTemplate(
                 template="Answer the user query.\n{format_instructions}\n{input}\n",
                 input_variables=["input"],
-                partial_variables={"format_instructions": parser.get_format_instructions()},
+                partial_variables={"format_instructions": parser.get_format_instructions()}
             )
             chain = self.prompt_template | self.model | parser
         else:
             chain = self.prompt_template | self.model | StrOutputParser()
-        
-        if self.response_format == "json":
-            if not self.json_keys:
-                json_example = {"key": "value"}
-            else:
-                json_example = {key: "value" for key in self.json_keys}
-            full_prompt += f"""Return a JSON object STRICTLY with the following keys: {self.json_keys}.
-            All keys must be present in the response.
-            e.g.:
-```json
-{json.dumps(json_example, indent=4)}
-```"""
-        
-
+            
         if self.response_format == "code":
             full_prompt += """Return only python code in Markdown format, e.g.:
 ```python
 ....
 ```"""
-
-        if self.response_format == "list":
-            full_prompt += """Return a comma separated list of items, e.g.:
-item1, item2, item3
-"""
         
         if image_paths is not None:
             self.prompt_template = ChatPromptTemplate.from_messages(
@@ -157,45 +152,14 @@ item1, item2, item3
         if self.response_format == "code":
             result = self._sanitize_output(result)
 
-        if self.response_format == "json" and self.json_keys:
-            for key in self.json_keys:
-                if key not in result:
-                    query = f""" 
-                    For the query: {query}, the following response was generated: {result}. It didn't follow the expected format containing the keys: {self.json_keys}. Please ensure that the response follows the expected format and contains all the keys.
-                    """
-                    logging.error(query)
-                    return None
-        
-        if self.response_format == "json":
-            result = self._adjust_json_types(str(result))
-
         if self.use_cache and not image_paths:
             self.cache.append(query, result)
         
+        if self.response_format == "json":
+            result = result.model_dump_json()
+        if self.response_format == "list":
+            result = result.response
         return result
-    
-    def _adjust_json_types(self, json_str):
-        """Adjust JSON types for None and boolean values."""
-        try:
-            json_obj = json.loads(json_str)
-            adjusted_json_str = json.dumps(json_obj, default=self._json_type_converter)
-            return adjusted_json_str
-        except json.JSONDecodeError:
-            logger.error("Failed to decode JSON response.")
-            return json_str
-
-    def _json_type_converter(self, obj):
-        """Convert JSON types to appropriate Python types."""
-        if obj is None:
-            return None
-        if obj == "True":
-            return True
-        elif obj == "False":
-            return False
-        elif obj == "None":
-            return None
-
-        return str(obj)
     
     def _get_prompt_with_history(self):
         """Constructs the full prompt including chat history."""
